74c844ee075d4f417cf4414b1e8ac08c756d0538
Carlos Banks <cbanks@clypr.co>
2020-08-15 13:47:03 -0400

    Completed missing question in comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -36,7 +36,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [user.name for user in humans if user.name[0] == 'C' or user.name[0] == 'D' or user.name[0] == 'E' or user.name[0] == 'F' or user.name[0] == 'G']
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
